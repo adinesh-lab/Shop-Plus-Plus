747aa2478ebbbf94a0a3a29313353b771d1e7dbc
adinesh-lab <124509633+adinesh-lab@users.noreply.github.com>
2024-07-04 14:34:10 +0530

    first commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>An emmorce website is an online platform that facilitates buying and selling of the products and services over the internet serves as a virtual market place where business and individuals can do sales An emmorce website is an online platform that facilitates buying and selling of the products and services over the internet serves as a virtual market place where business and individuals can do salesAn emmorce website is an online platform that facilitates buying and selling of the products and services over the internet serves as a virtual market place where business and individuals can do sales </p>
+                <p>An emmorce website is an online platform that facilitates buying and selling of the products and services over the internet serves as a virtual market place where business and individuals can do salesAn emmorce website is an online platform that facilitates buying and selling of the products and services over the internet serves as a virtual market place where business and individuals can do sales </p>
+            </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
